66e92d172b8b82e643a20a50aca14890964761ea
Rammas <luis.nieto.palacios1996@gmail.com>
2024-10-09 22:46:30 -0600

    Fix for visible mobile menus

--- src/navbar/verticalmenu/verticalmenu.tsx
+++ src/navbar/verticalmenu/verticalmenu.tsx
@@ -1,54 +1,64 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { getUserRoleFromString } from "../../apis/api_usuarios";
 import { useAuth } from "../../login/auth-provider/auth_provider";
 import menu_layout from "../../menus/menu_struct";
 
 function VerticalMenu() {
-    const { onLogout } = useAuth();
+  const { onLogout, userRole } = useAuth();
 
-    const menu_items = menu_layout.map((topic, index) =>
-        <React.Fragment key={topic.title + index}>
-            <p className="menu-label" key={topic.title}>{topic.title}</p>
-            <ul className="menu-list" key={index}>
-                {topic.children.map(item => {
-                    if ('children' in item) {
-                        return (
-                            item.children?.map(subitem =>
-                                <li key={subitem.title}>
-                                    <Link to={subitem.link} key={topic.title + subitem.title}>
-                                        {subitem.title}
-                                    </Link>
-                                </li>
-                            )
-                        );
-                    }
-                    else {
-                        return (
-                            <li key={item.title}>
-                                <Link to={item.link} key={topic.title + item.title}>
-                                    {item.title}
-                                </Link>
-                            </li>
-                        );
-                    }
-                })}
-            </ul>
-        </React.Fragment>
-    );
+  const userVisibility = getUserRoleFromString(userRole);
+
+  const menu_items = menu_layout.map((topic, index) => {
+    if (!topic.visibility.includes(userVisibility))
+      return <React.Fragment key={topic.title + index}></React.Fragment>;
 
     return (
-        <aside className="menu pl-5">
-            {menu_items}
-            <hr />
-            <ul className="menu-list mb-5">
-                <li>
-                    <Link to="/" onClick={() => onLogout()}>
-                        Log Out
-                    </Link>
+      <React.Fragment key={topic.title + index}>
+        <p className="menu-label" key={topic.title}>
+          {topic.title}
+        </p>
+        <ul className="menu-list" key={index}>
+          {topic.children.map((item) => {
+            if (!item.visibility.includes(userVisibility))
+              return <React.Fragment key={item.title} />;
+
+            if ("children" in item) {
+              return item.children?.map((subitem) => (
+                <li key={subitem.title}>
+                  <Link to={subitem.link} key={topic.title + subitem.title}>
+                    {subitem.title}
+                  </Link>
                 </li>
-            </ul>
-        </aside>
+              ));
+            } else {
+              return (
+                <li key={item.title}>
+                  <Link to={item.link} key={topic.title + item.title}>
+                    {item.title}
+                  </Link>
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </React.Fragment>
     );
+  });
+
+  return (
+    <aside className="menu pl-5">
+      {menu_items}
+      <hr />
+      <ul className="menu-list mb-5">
+        <li>
+          <Link to="/" onClick={() => onLogout()}>
+            Log Out
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  );
 }
 
-export default VerticalMenu
+export default VerticalMenu;
